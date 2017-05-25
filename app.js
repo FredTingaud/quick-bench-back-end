32fd0a1fd3e3bbadd225f550c61134659e152eca
TINGAUD Frederic <ftingaud@hotmail.com>
2017-05-25 20:36:47 +0200

    Add the possibility to force recalculation of the benchmark.

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ function write(fileName, code) {
     });        
 }
 
-function execute(fileName, compiler, optim, version) {
+function execute(fileName, compiler, optim, version, force) {
     let options = {
         timeout: 30000,
         killSignal: 'SIGKILL'
     }
     return new Promise((resolve, reject) => {
-        exec('./run-docker ' + fileName + ' ' + compiler + ' ' + optim + ' ' + version, options, function (err, stdout, stderr) {
+        exec('./run-docker ' + fileName + ' ' + compiler + ' ' + optim + ' ' + version + (force ? ' -f' : ''), options, function (err, stdout, stderr) {
             if (err) {
                 exec("./kill-docker " + fileName);
                 reject("\u001b[0m\u001b[0;1;31mError or timeout\u001b[0m\u001b[1m<br>" + stdout);
@@ -45,18 +45,19 @@ function execute(fileName, compiler, optim, version) {
     });
 }
 
-function treat(code, compiler, optim, version) {
+function treat(code, compiler, optim, version, force) {
     if (code.length > MAX_CODE_LENGTH) {
         return Promise.reject('\u001b[0m\u001b[0;1;31mError: Unauthorized code length.\u001b[0m\u001b[1m');
     }
     var fileName = '/tmp/' + sha1(code + compiler + optim + version);
     code = '#include <benchmark/benchmark_api.h>\n' + code + '\nBENCHMARK_MAIN()';
-    return Promise.resolve(write(fileName, code)).then(() => execute(fileName, compiler, optim, version));
+    return Promise.resolve(write(fileName, code)).then(() => execute(fileName, compiler, optim, version, force));
 }
 
 app.post('/', upload.array(), function (req, res) {
-    Promise.resolve(treat(req.body.code, req.body.compiler, req.body.optim, req.body.version))
-	.then((done) => res.json({ result: JSON.parse(done.res), message: done.stdout })).catch((err) => res.json({ message: err }));
+    Promise.resolve(treat(req.body.code, req.body.compiler, req.body.optim, req.body.version, req.body.force))
+        .then((done) => res.json({ result: JSON.parse(done.res), message: done.stdout }))
+        .catch((err) => res.json({ message: err }));
 })
 
 app.listen(3000, function() {
