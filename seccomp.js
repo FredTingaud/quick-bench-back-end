c8f05770c7a7e4c02b39619a56972354bdacd2e8
TINGAUD Frederic <ftingaud@hotmail.com>
2017-06-25 20:56:37 +0200

    Download docker default seccomp

--- seccomp.js
+++ seccomp.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-const fs = require('fs');
 
-let source = JSON.parse(fs.readFileSync('default.json'), 'utf8');
-source.syscalls[0].names.push('perf_event_open');
-fs.writeFileSync('seccomp.json', JSON.stringify(source));
+const fs = require('fs');
+const request = require('request');
+
+request('https://raw.githubusercontent.com/moby/moby/master/profiles/seccomp/default.json', function(err, res, body) {
+	let source = JSON.parse(body);
+	source.syscalls[0].names.push('perf_event_open');
+	fs.writeFileSync('seccomp.json', JSON.stringify(source, null, 2));
+});
