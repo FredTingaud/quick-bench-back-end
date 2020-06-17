104ec6f9de63688c882a2f8778b80f8e24071ab3
TINGAUD Frederic <ftingaud@hotmail.com>
2020-06-17 15:22:31 +0200

    Update system test for docker version of the scripts.

--- system-test/test.js
+++ system-test/test.js
@@ -1,32 +1,36 @@
-const app = require('../app');
-const expect = require('chai').expect
+const app = require('../app-quick');
+const expect = require('chai').expect;
 
 const version = process.env.QB_VERSION;
 
 describe('run docker with version', function () {
     it('should have benchmark results', async () => {
-	var request = {
-	    compiler: version,
-	    optim: 3,
-	    cppVersion: 17,
-	    isAnnotated: "true",
-	    force: "true"
-	};
-	expect(version).to.be.ok;
-	const done = await app.execute('system-test/testfile/test', request);
-	const parsed = JSON.parse(done.res);
-	expect(parsed.benchmarks).to.have.length(2);
-	expect(done.annotation).to.be.ok;
-	expect(done.annotation.split('\n')).to.have.lengthOf.above(7);
-	expect(done.annotation).to.have.string('BM_StringCreation');
-	expect(done.annotation).to.have.string('BM_StringCopy');
-	//	expect(done.stdout).to.be.empty; // Removed because of a Docker message on my Ubuntu version.
-	console.log(done.stdout);
-	expect(done.annotation).to.be.ok;
+        var request = {
+            options: {
+                compiler: version,
+                optim: 3,
+                cppVersion: 17,
+                isAnnotated: "true",
+                lib: "gnu"
+            },
+            force: "true"
+        };
+        expect(version).to.be.ok;
+        process.env.BENCH_ROOT = __dirname;
+        const done = await app.execute('system-test/testfile/test', request);
+        const parsed = JSON.parse(done.res);
+        expect(parsed.benchmarks).to.have.length(2);
+        expect(done.annotation).to.be.ok;
+        expect(done.annotation.split('\n')).to.have.lengthOf.above(7);
+        expect(done.annotation).to.have.string('BM_StringCreation');
+        expect(done.annotation).to.have.string('BM_StringCopy');
+        //	expect(done.stdout).to.be.empty; // Removed because of a Docker message on my Ubuntu version.
+        console.log(done.stdout);
+        expect(done.annotation).to.be.ok;
     }).timeout(120000);
-})
+});
 
-describe('check compiler version inside docker', function() {
+describe('check compiler version inside docker', function () {
     it('should have the same version', async () => {
         const exec = require('child_process').exec;
         let options = {
@@ -39,4 +43,4 @@ describe('check compiler version inside docker', function() {
         }));
         expect(result).to.eql(version);
     }).timeout(60000);
-})
+});
