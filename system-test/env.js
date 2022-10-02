eb1f5dfa241153e658b84a57920e06f22411cae0
TINGAUD Frederic <ftingaud@hotmail.com>
2022-10-02 12:28:14 +0200

    Rename about-me script.

--- system-test/env.js
+++ system-test/env.js
@@ -16,17 +16,17 @@ describe('check compiler version inside docker', function () {
     }).timeout(60000);
 });
 
-describe('check available standards', function() {
+describe('check available flags', function() {
     it('should contain multiple standards', async() =>{
         const exec = require('child_process').exec;
         let options = {
             timeout: 60000,
             killSignal: 'SIGKILL'
         };
-        const result = await new Promise(resolve => exec(`docker run --rm -t fredtingaud/quick-bench:${version} /bin/bash -c "./std-versions"`, options, (error, stdout, stderr) => {
+        const result = await new Promise(resolve => exec(`docker run --rm -t fredtingaud/quick-bench:${version} /bin/bash -c "./about-me"`, options, (error, stdout, stderr) => {
             resolve(stdout + stderr);
         }));
-        expect(result).to.not.be.empty();
-        expect(result).to.contain('c++11');
+        expect(result).to.be.a('string').and.satisfy(msg => msg.startsWith('[version]\n1\n[std]'));
+        expect(result).to.have.string('c++11');
     })
 })
