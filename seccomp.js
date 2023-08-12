97f1a46cb65f21efc4def15787770024ec0ee82c
TINGAUD Frederic <ftingaud@hotmail.com>
2023-08-12 17:26:59 +0200

    Remove dependency to request

--- seccomp.js
+++ seccomp.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const request = require('request');
+const https = require('https');
 
-request('https://raw.githubusercontent.com/moby/moby/master/profiles/seccomp/default.json', function(err, res, body) {
-	let source = JSON.parse(body);
-	source.syscalls[0].names.push('perf_event_open');
-	fs.writeFileSync('seccomp.json', JSON.stringify(source, null, 2));
+const req = https.request('https://raw.githubusercontent.com/moby/moby/master/profiles/seccomp/default.json', function (res) {
+	res.setEncoding('utf-8');
+	let body = '';
+	res.on('data', function (chunk) {
+		body = body + chunk;
+	});
+
+	res.on('end', () => {
+		let source = JSON.parse(body);
+		source.syscalls[0].names.push('perf_event_open');
+		fs.writeFileSync('seccomp.json', JSON.stringify(source, null, 2));
+	});
+}).on('error', (e) => {
+	console.error('Error while getting seccomp.js:\n' + e);
 });
+
+req.end();
