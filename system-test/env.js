f543d6ac73572f719a829fe1ccf3be7f17395d09
TINGAUD Frederic <ftingaud@hotmail.com>
2023-09-03 16:36:19 +0200

    Fix test dirname

--- system-test/env.js
+++ system-test/env.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import { exec } from 'child_process';
 
 const version = process.env.QB_VERSION;
+const dirname = import.meta.url.replace('file:/');
 
 describe('check compiler version inside docker', function () {
     it('should have the same version', async () => {
@@ -9,7 +10,7 @@ describe('check compiler version inside docker', function () {
             timeout: 60000,
             killSignal: 'SIGKILL'
         };
-        const result = await new Promise((resolve, reject) => exec(`docker run --rm -v ${import.meta.url}/env/version.cpp:/home/builder/bench-file.cpp -t fredtingaud/quick-bench:${version} /bin/bash -c "./build && ./run"`, options, (err, stdout, stderr) => {
+        const result = await new Promise((resolve, reject) => exec(`docker run --rm -v ${dirname}/env/version.cpp:/home/builder/bench-file.cpp -t fredtingaud/quick-bench:${version} /bin/bash -c "./build && ./run"`, options, (err, stdout, stderr) => {
             if (err) {
                 reject(stderr);
             } else {
